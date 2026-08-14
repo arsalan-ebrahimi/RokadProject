1290393cbecab7925375964405a12cb7cad63211
arsalan-ebrahimi <ebrahimi5432109@gmail.com>
2026-08-14 21:56:12 +0330

    Add Redux (Dashboard)

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import AuthContextProvider from './Context/AuthContext'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './Store/store'; 
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthContextProvider>
+    <Provider store={store}>
       <App />
-    </AuthContextProvider>
-  </StrictMode>,
-)
+    </Provider>
+  </StrictMode>
+);
