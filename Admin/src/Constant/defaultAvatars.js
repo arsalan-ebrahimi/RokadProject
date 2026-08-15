6ca1da7563a0e3a8b97958965b4f98cbe93f9f61
arsalan-ebrahimi <ebrahimi5432109@gmail.com>
2026-08-15 20:42:38 +0330

    Update defaultAvatars.js

--- Admin/src/Constant/defaultAvatars.js
+++ Admin/src/Constant/defaultAvatars.js
@@ -3,9 +3,10 @@
 // Description: List of predefined avatars stored in frontend public folder
 // ==========================================
 export const DEFAULT_AVATARS = [
-  { id: 1, filename: "default-avatar-1.png", alt: "آواتار ۱" },
-  { id: 2, filename: "default-avatar-2.png", alt: "آواتار ۲" },
-  { id: 3, filename: "default-avatar-3.png", alt: "آواتار ۳" },
-  { id: 4, filename: "default-avatar-4.png", alt: "آواتار ۴" },
-  { id: 5, filename: "default-avatar-5.png", alt: "آواتار ۵" },
+  { id: 1, filename: "default-BoyStudent.png", alt: "آواتار دانش آموز پسر" },
+  { id: 2, filename: "default-DadStudent.png", alt: "آواتار پدر دانش آموز" },
+  { id: 3, filename: "default-GirlStudent", alt: "آواتار  دانش آموز  دختر" },
+  { id: 4, filename: "default-MomStudent", alt: "آواتار مادر دانش آموز" },
+  { id: 5, filename: "defaultManTeacher", alt: "آواتار معلم مرد" },
+  { id: 5, filename: "defaultWomanTeacher", alt: "آواتار معلم زن"  },
 ];
