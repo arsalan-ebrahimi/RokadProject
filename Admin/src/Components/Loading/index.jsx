05a0f638ba8f206ce21bca049220825aebbbc88f
arsalan-ebrahimi <ebrahimi5432109@gmail.com>
2026-08-15 20:41:50 +0330

    Update Loading Component
    Can Set Custom color size margin

--- Admin/src/Components/Loading/index.jsx
+++ Admin/src/Components/Loading/index.jsx
@@ -1,9 +1,9 @@
 import { BeatLoader } from 'react-spinners';
 
-export default function Loading() {
+export default function Loading({ color = "#51b5a5", size = 8, margin = 2 }) {
   return (
     <div className='flex justify-center items-center'>
-      <BeatLoader size={8} color='#ffffff' margin={2} />
+      <BeatLoader size={size} color={color} margin={margin} />
     </div>
   );
 }
