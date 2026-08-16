202e70a2b038f0a34734f87f0bc04e393dfd7f43
arsalan-ebrahimi <ebrahimi5432109@gmail.com>
2026-08-17 01:47:18 +0330

    Update AwardCn.js

--- API/Modules/Award/AwardCn.js
+++ API/Modules/Award/AwardCn.js
@@ -20,7 +20,6 @@ export const create = catchAsync(async (req, res, next) => {
 
 export const getAll = catchAsync(async (req, res, next) => {
   const features = new ApiFeatures(Award, req.query, req.role)
-    .search()
     .filter()
     .sort()
     .limitFields()
@@ -34,6 +33,7 @@ export const getAll = catchAsync(async (req, res, next) => {
 export const getOne = catchAsync(async (req, res, next) => {
   const features = new ApiFeatures(Award, req.query, req.role)
     .addManualFilters({ _id: req.params.id })
+    .filter() 
     .limitFields()
     .populate();
 
