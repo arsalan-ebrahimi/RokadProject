e6e98c1b69012449f3e4b99dc6574f50d638a6b6
arsalan-ebrahimi <ebrahimi5432109@gmail.com>
2026-08-17 01:43:09 +0330

    Update Default avatars

--- Admin/src/Constants/defaultAvatars.js
+++ Admin/src/Constants/defaultAvatars.js
@@ -5,8 +5,8 @@
 export const DEFAULT_AVATARS = [
   { id: 1, filename: "default-BoyStudent.png", alt: "آواتار دانش آموز پسر" },
   { id: 2, filename: "default-DadStudent.png", alt: "آواتار پدر دانش آموز" },
-  { id: 3, filename: "default-GirlStudent", alt: "آواتار  دانش آموز  دختر" },
-  { id: 4, filename: "default-MomStudent", alt: "آواتار مادر دانش آموز" },
-  { id: 5, filename: "defaultManTeacher", alt: "آواتار معلم مرد" },
-  { id: 5, filename: "defaultWomanTeacher", alt: "آواتار معلم زن"  },
+  { id: 3, filename: "default-GirlStudent.png", alt: "آواتار  دانش آموز  دختر" },
+  { id: 4, filename: "default-MomStudent.png", alt: "آواتار مادر دانش آموز" },
+  { id: 5, filename: "defaultManTeacher.png", alt: "آواتار معلم مرد" },
+  { id: 6, filename: "defaultWomanTeacher.png", alt: "آواتار معلم زن"  },
 ];
